Sanky1612/no-OS

commit c9e00645575f1a02d46dfbb72cbf283324f6d265

projects: Add SWIOT1L project
Add the SWIOT1L project.

Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

projects/swiot1l/src.mk
@@ -1,0 +1,88 @@
+LIBRARIES += lwip
+TINYIIOD=y
+
+include $(PROJECT)/src/platform/$(PLATFORM)/platform_src.mk
+
+INCS += $(NO-OS)/network/tcp_socket.h
+INCS += $(NO-OS)/network/noos_mbedtls_config.h
+INCS += $(NO-OS)/network/network_interface.h
+
+SRCS += $(NO-OS)/network/tcp_socket.c
+
+SRCS += $(PROJECT)/src/platform/$(PLATFORM)/main.c
+
+INCS += $(PROJECT)/src/common/common_data.h
+SRCS += $(PROJECT)/src/common/common_data.c
+INCS += $(PROJECT)/src/common/swiot.h
+SRCS += $(PROJECT)/src/common/swiot.c
+
+INCS += $(PROJECT)/src/platform/platform_includes.h
+
+INCS += $(PROJECT)/src/platform/$(PLATFORM)/parameters.h
+SRCS += $(PROJECT)/src/platform/$(PLATFORM)/parameters.c 
+
+INCS += $(INCLUDE)/no_os_delay.h     \
+		$(INCLUDE)/no_os_error.h     \
+		$(INCLUDE)/no_os_gpio.h      \
+		$(INCLUDE)/no_os_print_log.h \
+		$(INCLUDE)/no_os_spi.h       \
+		$(INCLUDE)/no_os_irq.h      \
+		$(INCLUDE)/no_os_init.h      \
+		$(INCLUDE)/no_os_list.h      \
+		$(INCLUDE)/no_os_mutex.h      \
+		$(INCLUDE)/no_os_crc8.h      \
+		$(INCLUDE)/no_os_uart.h      \
+		$(INCLUDE)/no_os_i2c.h      \
+		$(INCLUDE)/no_os_mdio.h      \
+		$(INCLUDE)/no_os_timer.h      \
+		$(INCLUDE)/no_os_lf256fifo.h \
+		$(INCLUDE)/no_os_util.h \
+		$(INCLUDE)/no_os_units.h \
+		$(INCLUDE)/no_os_alloc.h
+
+SRCS += $(DRIVERS)/api/no_os_gpio.c \
+		$(NO-OS)/util/no_os_lf256fifo.c \
+		$(DRIVERS)/api/no_os_irq.c  \
+		$(DRIVERS)/api/no_os_spi.c  \
+		$(DRIVERS)/api/no_os_uart.c \
+		$(DRIVERS)/api/no_os_i2c.c \
+		$(DRIVERS)/api/no_os_timer.c \
+		$(DRIVERS)/api/no_os_mdio.c \
+		$(NO-OS)/util/no_os_list.c \
+		$(NO-OS)/util/no_os_crc8.c \
+		$(NO-OS)/util/no_os_util.c \
+		$(NO-OS)/util/no_os_alloc.c
+
+INCS += $(DRIVERS)/adc-dac/ad74413r/ad74413r.h
+SRCS += $(DRIVERS)/adc-dac/ad74413r/ad74413r.c
+
+INCS += $(DRIVERS)/digital-io/max14906/max14906.h
+SRCS += $(DRIVERS)/digital-io/max14906/max14906.c
+
+INCS += $(DRIVERS)/temperature/adt75/adt75.h
+SRCS += $(DRIVERS)/temperature/adt75/adt75.c
+
+INCS += $(DRIVERS)/adc-dac/ad74413r/iio_ad74413r.h
+SRCS += $(DRIVERS)/adc-dac/ad74413r/iio_ad74413r.c
+SRCS += $(DRIVERS)/adc-dac/ad74413r/iio_ad74413r_trig.c
+
+INCS += $(DRIVERS)/digital-io/max14906/iio_max14906.h
+SRCS += $(DRIVERS)/digital-io/max14906/iio_max14906.c
+
+INCS += $(DRIVERS)/temperature/adt75/iio_adt75.h
+SRCS += $(DRIVERS)/temperature/adt75/iio_adt75.c
+
+INCS += $(INCLUDE)/no_os_crc8.h
+INCS += $(DRIVERS)/net/adin1110/adin1110.h
+INCS += $(NO-OS)/network/lwip_raw_socket/netdevs/adin1110/lwip_adin1110.h
+SRCS += $(NO-OS)/network/lwip_raw_socket/netdevs/adin1110/lwip_adin1110.c
+SRCS += $(DRIVERS)/net/adin1110/adin1110.c
+SRCS += $(NO-OS)/util/no_os_crc8.c
+
+SRCS += $(PROJECT)/src/swiot_fw.c
+INCS += $(PROJECT)/src/swiot_fw.h
+
+SRC_DIRS += $(NO-OS)/iio/iio_app
+
+SRCS += $(NO-OS)/iio/iio_trigger.c
+INCS += $(NO-OS)/iio/iio_trigger.h

projects/swiot1l/Makefile
@@ -1,0 +1,13 @@
+PLATFORM = maxim
+TARGET = max32650
+
+include ../../tools/scripts/generic_variables.mk
+
+include src.mk
+
+include ../../tools/scripts/generic.mk
+
+CFLAGS += -DNO_OS_LWIP_NETWORKING
+CFLAGS += -DNO_OS_STATIC_IP
+CFLAGS += -DIIO_IGNORE_BUFF_OVERRUN_ERR
+CFLAGS += -DNO_OS_LWIP_INIT_ONETIME=1

projects/swiot1l/src/common/common_data.c
@@ -1,0 +1,314 @@
+/***************************************************************************//**
+ *   @file   common_data.c
+ *   @brief  Defines common data to be used by the SWIOT1L firmware.
+ *   @author Ciprian Regus (ciprian.regus@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#include "common_data.h"
+
+struct no_os_uart_init_param uart_ip = {
+	.device_id = UART_DEVICE_ID,
+	.irq_id = UART_IRQ_ID,
+	.asynchronous_rx = true,
+	.baud_rate = UART_BAUDRATE,
+	.size = NO_OS_UART_CS_8,
+	.parity = NO_OS_UART_PAR_NO,
+	.platform_ops = &max_uart_ops,
+	.stop = NO_OS_UART_STOP_1_BIT,
+	.extra = UART_EXTRA,
+};
+
+const struct no_os_spi_init_param adin1110_spi_ip = {
+	.device_id = 2,
+	.max_speed_hz = 15000000,
+	.bit_order = NO_OS_SPI_BIT_ORDER_MSB_FIRST,
+	.mode = NO_OS_SPI_MODE_0,
+	.platform_ops = &max_spi_ops,
+	.chip_select = 0,
+	.extra = SPI_EXTRA,
+};
+
+const struct no_os_i2c_init_param adt75_comm_param = {
+	.device_id = 1,
+	.max_speed_hz = 400000,
+	.slave_address = ADT75_ADDR(0),
+	.platform_ops = &max_i2c_ops,
+	.extra = I2C_EXTRA,
+};
+
+struct adt75_init_param adt75_ip = {
+	.comm_param = adt75_comm_param,
+};
+
+const struct no_os_gpio_init_param adin1110_int_ip = {
+	.port = 2,
+	.number = 6,
+	.pull = NO_OS_PULL_UP,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+const struct no_os_gpio_init_param adin1110_rst_gpio_ip = {
+	.port = 2,
+	.number = 1,
+	.pull = NO_OS_PULL_UP,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+const struct no_os_gpio_init_param tx_perf_gpio_ip = {
+	.port = 2,
+	.number = 16,
+	.pull = NO_OS_PULL_UP,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+struct no_os_gpio_init_param ad74413r_reset_ip = {
+	.port = 0,
+	.number = 26,
+	.pull = NO_OS_PULL_UP,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+const struct no_os_gpio_init_param ad74413r_ldac_ip = {
+	.port = 0,
+	.number = 27,
+	.pull = NO_OS_PULL_NONE,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+const struct no_os_gpio_init_param ad74413r_irq_ip = {
+	.port = 1,
+	.number = 22,
+	.pull = NO_OS_PULL_UP,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+const struct no_os_gpio_init_param max14906_en_ip = {
+	.port = 1,
+	.number = 25,
+	.pull = NO_OS_PULL_NONE,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+const struct no_os_gpio_init_param max14906_d1_ip = {
+	.port = 3,
+	.number = 7,
+	.pull = NO_OS_PULL_NONE,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+const struct no_os_gpio_init_param max14906_d2_ip = {
+	.port = 3,
+	.number = 4,
+	.pull = NO_OS_PULL_NONE,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+const struct no_os_gpio_init_param max14906_d3_ip = {
+	.port = 3,
+	.number = 8,
+	.pull = NO_OS_PULL_NONE,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+const struct no_os_gpio_init_param max14906_d4_ip = {
+	.port = 3,
+	.number = 5,
+	.pull = NO_OS_PULL_NONE,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+const struct no_os_gpio_init_param max14906_synch_ip = {
+	.port = 3,
+	.number = 6,
+	.pull = NO_OS_PULL_NONE,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+const struct no_os_gpio_init_param adin1110_swpd_ip = {
+	.port = 2,
+	.number = 25,
+	.pull = NO_OS_PULL_UP,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+const struct no_os_gpio_init_param adin1110_tx2p4_ip = {
+	.port = 2,
+	.number = 10,
+	.pull = NO_OS_PULL_DOWN,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+const struct no_os_gpio_init_param adin1110_mssel_ip = {
+	.port = 2,
+	.number = 9,
+	.pull = NO_OS_PULL_NONE,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+const struct no_os_gpio_init_param adin1110_cfg0_ip = {
+	.port = 2,
+	.number = 3,
+	.pull = NO_OS_PULL_NONE,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+const struct no_os_gpio_init_param adin1110_cfg1_ip = {
+	.port = 2,
+	.number = 0,
+	.pull = NO_OS_PULL_UP,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+const struct no_os_gpio_init_param swiot_led1_ip = {
+	.port = 2,
+	.number = 15,
+	.pull = NO_OS_PULL_UP,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+const struct no_os_gpio_init_param swiot_led2_ip = {
+	.port = 2,
+	.number = 14,
+	.pull = NO_OS_PULL_UP,
+	.platform_ops = &max_gpio_ops,
+	.extra = GPIO_EXTRA,
+};
+
+struct adin1110_init_param adin1110_ip = {
+	.chip_type = ADIN1110,
+	.comm_param = adin1110_spi_ip,
+	.reset_param = adin1110_rst_gpio_ip,
+	.append_crc = false,
+};
+
+const struct no_os_spi_init_param ad74413r_spi_ip = {
+	.device_id = 3,
+	.max_speed_hz = 15000000,
+	.bit_order = NO_OS_SPI_BIT_ORDER_MSB_FIRST,
+	.mode = NO_OS_SPI_MODE_2,
+	.platform_ops = &max_spi_ops,
+	.chip_select = 3,
+	.extra = SPI_EXTRA,
+};
+
+struct no_os_spi_init_param max14906_spi_ip = {
+	.device_id = 1,
+	.max_speed_hz = 10000000,
+	.bit_order = NO_OS_SPI_BIT_ORDER_MSB_FIRST,
+	.mode = NO_OS_SPI_MODE_0,
+	.platform_ops = &max_spi_ops,
+	.chip_select = 3,
+	.extra = SPI_EXTRA,
+};
+
+const struct no_os_spi_init_param pmod_spi_ip = {
+	.device_id = 1,
+	.max_speed_hz = 10000000,
+	.bit_order = NO_OS_SPI_BIT_ORDER_MSB_FIRST,
+	.mode = NO_OS_SPI_MODE_0,
+	.platform_ops = &max_spi_ops,
+	.chip_select = 2,
+	.extra = SPI_EXTRA,
+};
+
+const struct no_os_gpio_init_param psu_gpio_ip = {
+	.port = 3,
+	.number = 9,
+	.platform_ops = &max_gpio_ops,
+	.pull = NO_OS_PULL_UP,
+	.extra = GPIO_EXTRA
+};
+
+struct swiot_iio_desc_init_param swiot_ip = {
+	.psu_gpio_param = psu_gpio_ip,
+	.identify_gpio_param = swiot_led2_ip,
+	.mode = SWIOT_CONFIG,
+};
+
+struct max14906_init_param max14906_ip = {
+	.chip_address = 0,
+	.comm_param = &max14906_spi_ip,
+	.crc_en = true,
+};
+
+const struct no_os_irq_init_param ad74413r_nvic_ip = {
+	.irq_ctrl_id = 0,
+	.platform_ops = &max_irq_ops,
+	.extra = NULL,
+};
+
+struct ad74413r_init_param ad74413r_ip = {
+	.chip_id = AD74413R,
+	.comm_param = ad74413r_spi_ip,
+	.reset_gpio_param = &ad74413r_reset_ip,
+};
+
+/* GPIO trigger */
+struct no_os_irq_init_param ad74413r_gpio_irq_ip = {
+	.irq_ctrl_id = GPIO_IRQ_ID,
+	.platform_ops = &max_gpio_irq_ops,
+	.extra = GPIO_IRQ_EXTRA,
+};
+
+const struct iio_hw_trig_cb_info gpio_cb_info = {
+	.event = NO_OS_EVT_GPIO,
+	.peripheral = NO_OS_GPIO_IRQ,
+	.handle = AD74413R_GPIO_CB_HANDLE,
+};
+
+struct iio_hw_trig_init_param ad74413r_gpio_trig_ip = {
+	.irq_id = AD74413R_GPIO_TRIG_IRQ_ID,
+	.irq_trig_lvl = NO_OS_IRQ_EDGE_FALLING,
+	.cb_info = gpio_cb_info,
+	.name = AD74413R_GPIO_TRIG_NAME,
+};

projects/swiot1l/src/common/common_data.h
@@ -1,0 +1,90 @@
+/***************************************************************************//**
+ *   @file   common_data.h
+ *   @brief  Defines common data to be used by the swiot1l project.
+ *   @author Ciprian Regus (ciprian.regus@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#ifndef __COMMON_DATA_H__
+#define __COMMON_DATA_H__
+
+#include "platform_includes.h"
+#include "ad74413r.h"
+#include "swiot.h"
+#include "max14906.h"
+#include "adin1110.h"
+#include "adt75.h"
+#include "iio_trigger.h"
+
+#define AD74413R_GPIO_TRIG_NAME "ad74413r-dev0"
+
+extern struct no_os_uart_init_param uart_ip;
+extern struct adin1110_init_param adin1110_ip;
+extern const struct no_os_spi_init_param adin1110_spi_ip;
+extern const struct no_os_spi_init_param ad74413r_spi_ip;
+extern struct no_os_spi_init_param max14906_spi_ip;
+extern struct swiot_iio_desc_init_param swiot_ip;
+extern struct max14906_init_param max14906_ip;
+extern struct ad74413r_init_param ad74413r_ip;
+extern struct adt75_init_param adt75_ip;
+extern const struct no_os_gpio_init_param adin1110_int_gpio_ip;
+extern const struct no_os_gpio_init_param tx_perf_gpio_ip;
+extern const struct no_os_gpio_init_param rx_perf_gpio_ip;
+extern const struct no_os_gpio_init_param adin1110_rst_gpio_ip;
+extern const struct no_os_gpio_init_param adin1110_swpd_ip;
+extern const struct no_os_gpio_init_param adin1110_tx2p4_ip;
+extern const struct no_os_gpio_init_param adin1110_mssel_ip;
+extern const struct no_os_gpio_init_param adin1110_cfg1_ip;
+extern const struct no_os_gpio_init_param adin1110_cfg0_ip;
+extern const struct no_os_gpio_init_param adin1110_int_ip;
+extern const struct no_os_gpio_init_param swiot_led1_ip;
+extern const struct no_os_gpio_init_param swiot_led2_ip;
+extern const struct no_os_gpio_init_param psu_gpio_ip;
+extern struct no_os_gpio_init_param ad74413r_reset_ip;
+extern const struct no_os_gpio_init_param ad74413r_ldac_ip;
+extern const struct no_os_gpio_init_param ad74413r_irq_ip;
+extern const struct no_os_gpio_init_param max14906_en_ip;
+extern const struct no_os_gpio_init_param max14906_d1_ip;
+extern const struct no_os_gpio_init_param max14906_d2_ip;
+extern const struct no_os_gpio_init_param max14906_d3_ip;
+extern const struct no_os_gpio_init_param max14906_d4_ip;
+extern const struct no_os_gpio_init_param max14906_synch_ip;
+extern const struct no_os_irq_init_param ad74413r_nvic_ip;
+
+extern struct iio_trigger ad74413r_iio_trig_desc;
+extern struct iio_trigger swiot_trigger;
+extern struct iio_hw_trig_init_param ad74413r_gpio_trig_ip;
+extern struct no_os_irq_init_param ad74413r_gpio_irq_ip;
+
+#endif /* __COMMON_DATA_H__ */

projects/swiot1l/src/common/swiot.h
@@ -1,0 +1,113 @@
+/***************************************************************************//**
+ *   @file   swiot.h
+ *   @brief  Header file for the swiot IIO device.
+ *   @author Ciprian Regus (ciprian.regus@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#ifndef IIO_SWIOT_H
+#define IIO_SWIOT_H
+
+#include <stdbool.h>
+#include "no_os_gpio.h"
+#include "ad74413r.h"
+#include "iio_ad74413r.h"
+#include "max14906.h"
+#include "adin1110.h"
+
+#define SWIOT_CHANNELS	4
+
+static const char *const ad74413r_function_available[] = {
+	[AD74413R_HIGH_Z] = "high_z",
+	[AD74413R_VOLTAGE_OUT] = "voltage_out",
+	[AD74413R_CURRENT_OUT] = "current_out",
+	[AD74413R_VOLTAGE_IN] = "voltage_in",
+	[AD74413R_CURRENT_IN_EXT] = "current_in_ext",
+	[AD74413R_CURRENT_IN_LOOP] = "current_in_loop",
+	[AD74413R_RESISTANCE] = "resistance",
+	[AD74413R_DIGITAL_INPUT] = "digital_input",
+	[AD74413R_DIGITAL_INPUT_LOOP] = "digital_input_loop",
+	[AD74413R_CURRENT_IN_EXT_HART] = "current_in_ext_hart",
+	[AD74413R_CURRENT_IN_LOOP_HART] = "current_in_loop_hart"
+};
+
+enum swiot_device {
+	SWIOT_AD74413R,
+	SWIOT_MAX14906,
+	SWIOT_DEV_CNT,
+};
+
+enum swiot_mode {
+	SWIOT_CONFIG,
+	SWIOT_RUNTIME,
+};
+
+struct swiot_config_state {
+	bool enabled;
+	enum swiot_device device;
+	uint32_t function;
+};
+
+struct swiot_iio_desc {
+	struct ad74413r_iio_desc *ad74413r;
+	struct max14906_iio_desc *max14906;
+	struct adin1110_desc *adin1110;
+	struct iio_device *iio_dev;
+	uint32_t active_channels;
+	uint8_t no_of_active_channels;
+
+	enum swiot_mode mode;
+	bool mode_change;
+	struct no_os_gpio_desc *psu_gpio;
+	struct no_os_gpio_desc *identify_gpio;
+
+	struct max14906_ch_config max14906_configs[MAX14906_CHANNELS];
+	struct ad74413r_channel_config ad74413r_configs[AD74413R_N_CHANNELS];
+	struct ad74413r_diag_channel_config
+		ad74413r_diag_configs[AD74413R_N_DIAG_CHANNELS];
+};
+
+struct swiot_iio_desc_init_param {
+	struct ad74413r_iio_desc *ad74413r;
+	struct max14906_iio_desc *max14906;
+	struct no_os_gpio_init_param psu_gpio_param;
+	struct no_os_gpio_init_param identify_gpio_param;
+	enum swiot_mode mode;
+};
+
+int swiot_iio_init(struct swiot_iio_desc **,
+		   struct swiot_iio_desc_init_param *);
+int swiot_iio_remove(struct swiot_iio_desc *);
+
+#endif

projects/swiot1l/src/platform/maxim/main.c
@@ -1,0 +1,66 @@
+/***************************************************************************//**
+ *   @file   main.c
+ *   @brief  Main file for Maxim platform of the swiot1l project.
+ *   @author Ciprian Regus (ciprian.regus@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#include "platform_includes.h"
+#include "common_data.h"
+#include "no_os_error.h"
+#include "no_os_init.h"
+
+#include "swiot_fw.h"
+
+/***************************************************************************//**
+ * @brief Main function
+ *
+ * @return ret - Result of the enabled examples execution.
+*******************************************************************************/
+int main()
+{
+	struct no_os_uart_desc *uart_desc;
+	int ret;
+
+	ret = no_os_init();
+	if (ret)
+		return ret;
+
+	ret = no_os_uart_init(&uart_desc, &uart_ip);
+	if (ret)
+		return ret;
+
+	no_os_uart_stdio(uart_desc);
+	return swiot_firmware();
+}

projects/swiot1l/src/platform/maxim/parameters.c
@@ -1,0 +1,59 @@
+/***************************************************************************//**
+ *   @file   parameters.c
+ *   @brief  Definition of Maxim platform data used by the swiot1l project.
+ *   @author Ciprian Regus (ciprian.regus@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#include "parameters.h"
+
+struct max_uart_init_param adin1110_uart_extra_ip = {
+	.flow = UART_FLOW_DIS,
+	.vssel = 1
+};
+
+struct max_spi_init_param adin1110_spi_extra_ip  = {
+	.num_slaves = 1,
+	.polarity = SPI_SS_POL_LOW,
+	.vssel = MXC_GPIO_VSSEL_VDDIOH,
+};
+
+struct max_gpio_init_param vddioh_gpio_extra = {
+	.vssel = MXC_GPIO_VSSEL_VDDIOH,
+};
+
+struct max_i2c_init_param vddioh_i2c_extra = {
+	.vssel = MXC_GPIO_VSSEL_VDDIOH,
+};

commit 577913baaa05ceeca001ebbb999a4509f7ffe4bb

projects:swiot1l: Add mutex source
The mutex files were missing from src.mk. Add them.

Fixes: c9e00645575f ("projects: Add SWIOT1L project")
Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

projects/swiot1l/src.mk
@@ -32,6 +32,7 @@ INCS += $(INCLUDE)/no_os_delay.h     \
 		$(INCLUDE)/no_os_mutex.h      \
 		$(INCLUDE)/no_os_crc8.h      \
 		$(INCLUDE)/no_os_uart.h      \
+		$(INCLUDE)/no_os_mutex.h      \
 		$(INCLUDE)/no_os_i2c.h      \
 		$(INCLUDE)/no_os_mdio.h      \
 		$(INCLUDE)/no_os_timer.h      \
@@ -51,6 +52,7 @@ SRCS += $(DRIVERS)/api/no_os_gpio.c \
 		$(NO-OS)/util/no_os_list.c \
 		$(NO-OS)/util/no_os_crc8.c \
 		$(NO-OS)/util/no_os_util.c \
+		$(NO-OS)/util/no_os_mutex.c \
 		$(NO-OS)/util/no_os_alloc.c
 
 INCS += $(DRIVERS)/adc-dac/ad74413r/ad74413r.h